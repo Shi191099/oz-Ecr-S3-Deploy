ded1dc4dc7f9388fae3a3e79b7c248e7cd45ef32
Shi <68201193+Shi191099@users.noreply.github.com>
2022-12-05 16:23:30 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -12,6 +12,7 @@ aws s3 cp s3://oz-s3-ec2-04/sample.war .
 
 yum install docker -y
 service docker start
+chmod 777 /var/run/docker.sock
 service docker status
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 805392809179.dkr.ecr.ap-southeast-2.amazonaws.com
