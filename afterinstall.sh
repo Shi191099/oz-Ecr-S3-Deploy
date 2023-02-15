b820c602d0daca585ff6c52c24d5f64cf724278b
Shi <68201193+Shi191099@users.noreply.github.com>
2023-02-15 15:59:56 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -4,11 +4,11 @@ mkdir /home/ec2-user/oz
 cd /home/ec2-user/oz
 
 echo "setting source bucket credentials"
-aws configure set aws_access_key_id Access_Key
-aws configure set aws_secret_access_key Secret_Key
+aws configure set aws_access_key_id $Access_Key
+aws configure set aws_secret_access_key $Secret_Key
 echo "credentials set successful"
 
-aws s3 cp s3://oz-s3-ec2-04/sample.war .
+# aws s3 cp s3://oz-s3-ec2-04/sample.war .
 
 yum install docker -y
 service docker start
@@ -19,4 +19,5 @@ aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS
 
 docker pull $account_id.dkr.ecr.$region.amazonaws.com/oz-pipeline-04:latest
 
-docker run -td -v sample.war:/data -P $account_id.dkr.ecr.$region.amazonaws.com/oz-pipeline-04:latest
+# docker run -td -v sample.war:/data -P $account_id.dkr.ecr.$region.amazonaws.com/oz-pipeline-04:latest
+docker run -td -P $account_id.dkr.ecr.$region.amazonaws.com/oz-pipeline-04:latest
